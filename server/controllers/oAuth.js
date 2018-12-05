6b86643983d6b76c617bdaf353362a3d8fd2709a
555032 <charlesprindle@c02x910hjgh7.users.bah.com>
2018-12-05 09:35:41 -0500

    change from code to token

--- server/controllers/oAuth.js
+++ server/controllers/oAuth.js
@@ -8,13 +8,25 @@ module.exports = {
         res.status(200).json({
             clientId: clientId,
             authDomain: authDomain,
-            authUrl: authDomain + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=code",
+            authUrl: authDomain + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&response_type=token",
             success: true,
             message: 'Successfully retrieved client ID.'
         })
+    },
+
+    getToken(req, res) {
+        const encoded = process.emitWarning.encoded_client_id_client_secret;
+        const authDomain = process.env.auth_domain;
+        const redirectUri = process.env.redirect_uri;
+        rp.post({
+            headers: {
+                Authorization: Basic,
+                encoded,
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            url: authDomain + "/oauth2/get_token",
+            body: "grant_type=authorization_code&redirect_uri=" + redirectUri + "&code=" + req.body.code
+
+        }).then((postRes) => {});
     }
 }
-
-
-// "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9WmV5Mjg1VmZKcFR4JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWQ3--&redirect_uri=https://api.login.yahoo.com/oauth2/request_auth&response_type=code",
-// "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9ak5IZ2x5WmNsaHp6JmQ9WVdrOVNqQkJUMnRYTjJrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1hYQ--&redirect_uri=oob&response_type=code&language=en-us
